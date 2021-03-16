4f1778f6d4a349134f8b9e24c60f643a314f15f3
vivek <vivek@athenaeum.in>
2021-03-16 14:30:52 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -3,6 +3,7 @@ package in.athenaeum;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Sample sample1 = new Sample();
+	    Sample sample2 = new Sample();  // how do I prevent this?
     }
 }
