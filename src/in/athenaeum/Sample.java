4798c1dbb82762bb657ba526fbabd0eb1de293ba
vivek <vivek@athenaeum.in>
2021-03-16 14:55:09 +0530

    done

--- src/in/athenaeum/Sample.java
+++ src/in/athenaeum/Sample.java
@@ -1,12 +1,12 @@
 package in.athenaeum;
 
 public class Sample {
-    private static Sample instance;
+    private volatile static Sample instance;
 
     private Sample() {
     }
 
-    public static Sample getInstance() {
+    public synchronized static Sample getInstance() {
         if (instance == null) {
             instance = new Sample();
             return instance;
